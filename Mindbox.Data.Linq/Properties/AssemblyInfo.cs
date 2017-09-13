965dfa44a551be189f0ea06a1b66b559309c9712
Shaykhullin Sergey <shaykhullin@mindbox.ru>
2017-09-13 17:11:02 +0300

    BugzId: 175218 Добавил nternalsVisibleTo для Moq'a

--- Mindbox.Data.Linq/Properties/AssemblyInfo.cs
+++ Mindbox.Data.Linq/Properties/AssemblyInfo.cs
@@ -4,6 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Security;
 
 [assembly: InternalsVisibleTo("Mindbox.Data.Linq.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 [assembly: SecurityTransparent]
 [assembly: AllowPartiallyTrustedCallers]
 [assembly: SecurityRules(SecurityRuleSet.Level1, SkipVerificationInFullTrust = true)]
