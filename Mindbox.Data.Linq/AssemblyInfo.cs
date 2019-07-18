20f4c5fa47570e755319fe9a550d3c0137c02a16
Rostislav Listerenko <listerenko@mindbox.ru>
2019-07-18 15:37:55 +0300

    Поправил пакет

--- Mindbox.Data.Linq/AssemblyInfo.cs
+++ Mindbox.Data.Linq/AssemblyInfo.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Security;
 
@@ -8,7 +6,3 @@ using System.Security;
 [assembly: SecurityTransparent]
 [assembly: AllowPartiallyTrustedCallers]
 [assembly: SecurityRules(SecurityRuleSet.Level1, SkipVerificationInFullTrust = true)]
-[assembly: AssemblyTitle("Mindbox.Data.Linq")]
-[assembly: AssemblyCompany("Mindbox")]
-[assembly: AssemblyDescription("A clone of Microsoft System.Data.Linq to allow multi-DLL extensibility and EF compatibility.")]
-[assembly: AssemblyVersion("2.0.7")]
