05cda35b2f2531cce31008f84f85b8d1689e281b
Leonid Gureev <gureev@mindbox.ru>
2021-07-12 15:59:07 +0300

    Use System.Data.SqlClient. (#35)
    * Use System.Data.SqlClient.

--- Mindbox.Data.Linq/SqlClient/Query/SqlFormatter.cs
+++ Mindbox.Data.Linq/SqlClient/Query/SqlFormatter.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Data.Linq.Mapping;
 using System.Data.Linq.Provider;
 using System.Linq;
-using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
 using System.Diagnostics.CodeAnalysis;
 
 namespace System.Data.Linq.SqlClient {
