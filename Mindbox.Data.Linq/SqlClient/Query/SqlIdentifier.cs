5a48bc6af576ca4333c0b4e1ff431d5809e5ec8f
Rostislav Listerenko <listerenko@mindbox.cloud>
2023-01-16 19:29:29 +0400

    try microsoft.data.client again

--- Mindbox.Data.Linq/SqlClient/Query/SqlIdentifier.cs
+++ Mindbox.Data.Linq/SqlClient/Query/SqlIdentifier.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Linq.Mapping;
 using System.Data.Linq.Provider;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Globalization;
 using System.Linq;
 using System.Text;
