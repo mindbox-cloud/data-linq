15ae43244950e3c482ab42a91b4c054134164ca0
Rostislav Listerenko <listerenko@mindbox.cloud>
2023-01-18 18:54:10 +0400

    revert microsoft.data.client

--- Mindbox.Data.Linq/SqlClient/Common/Expressions.cs
+++ Mindbox.Data.Linq/SqlClient/Common/Expressions.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
 using System.Linq.Expressions;
 using System.Linq;
 using System.Reflection;
--- Mindbox.Data.Linq/SqlClient/SqlBuilder.cs
+++ Mindbox.Data.Linq/SqlClient/SqlBuilder.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
