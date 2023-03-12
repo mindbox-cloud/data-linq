dcda16eeb25a2c5059347e509a63c972de23d417
TimurMannapov <mannapov@mindbox.cloud>
2023-03-13 01:03:31 +0300

    Draft

--- Mindbox.Data.Linq.Tests/MultiStatementQuery/SqlQueryTranslator.cs
+++ Mindbox.Data.Linq.Tests/MultiStatementQuery/SqlQueryTranslator.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Mindbox.Data.Linq.Tests.MultiStatementQuery;
 
@@ -28,46 +29,138 @@ class SqlQueryTranslator
     private static void TransalateCore(TranslationContext context, Expression node)
     {
         var items = GetExpressionChains(node).ToArray();
+        foreach (var item in items)
+            MapExpressions(context, item);
+    }
+
+    private static void MapExpressions(TranslationContext context, IExpressionEnumeratorItem itemAndStack)
+    {
+        var expression = itemAndStack.Expression;
+        if (context.Mapping.ContainsKey(expression))
+            return;
+        switch (itemAndStack.Expression.NodeType)
+        {
+            case ExpressionType.Constant:
+                var tableName = GetTableName((ConstantExpression)expression);
+                if (!string.IsNullOrEmpty(tableName))
+                    throw new NotSupportedException();
+                context.AddMapping(expression, new SqlTableNode(tableName));
+                break;
+            case ExpressionType.Parameter:
+            case ExpressionType.Quote:
+            case ExpressionType.Lambda:
+            case ExpressionType.Equal:
+            case ExpressionType.MemberAccess:
+            case ExpressionType.Call:
+            case ExpressionType.Add:
+            case ExpressionType.AddChecked:
+            case ExpressionType.And:
+            case ExpressionType.AndAlso:
+            case ExpressionType.ArrayLength:
+            case ExpressionType.ArrayIndex:
+            case ExpressionType.Coalesce:
+            case ExpressionType.Conditional:
+            case ExpressionType.Convert:
+            case ExpressionType.ConvertChecked:
+            case ExpressionType.Divide:
+            case ExpressionType.ExclusiveOr:
+            case ExpressionType.GreaterThan:
+            case ExpressionType.GreaterThanOrEqual:
+            case ExpressionType.Invoke:
+            case ExpressionType.LeftShift:
+            case ExpressionType.LessThan:
+            case ExpressionType.LessThanOrEqual:
+            case ExpressionType.ListInit:
+            case ExpressionType.MemberInit:
+            case ExpressionType.Modulo:
+            case ExpressionType.Multiply:
+            case ExpressionType.MultiplyChecked:
+            case ExpressionType.Negate:
+            case ExpressionType.UnaryPlus:
+            case ExpressionType.NegateChecked:
+            case ExpressionType.New:
+            case ExpressionType.NewArrayInit:
+            case ExpressionType.NewArrayBounds:
+            case ExpressionType.Not:
+            case ExpressionType.NotEqual:
+            case ExpressionType.Or:
+            case ExpressionType.OrElse:
+            case ExpressionType.Power:
+            case ExpressionType.RightShift:
+            case ExpressionType.Subtract:
+            case ExpressionType.SubtractChecked:
+            case ExpressionType.TypeAs:
+            case ExpressionType.TypeIs:
+            case ExpressionType.Assign:
+            case ExpressionType.Block:
+            case ExpressionType.DebugInfo:
+            case ExpressionType.Decrement:
+            case ExpressionType.Dynamic:
+            case ExpressionType.Default:
+            case ExpressionType.Extension:
+            case ExpressionType.Goto:
+            case ExpressionType.Increment:
+            case ExpressionType.Index:
+            case ExpressionType.Label:
+            case ExpressionType.RuntimeVariables:
+            case ExpressionType.Loop:
+            case ExpressionType.Switch:
+            case ExpressionType.Throw:
+            case ExpressionType.Try:
+            case ExpressionType.Unbox:
+            case ExpressionType.AddAssign:
+            case ExpressionType.AndAssign:
+            case ExpressionType.DivideAssign:
+            case ExpressionType.ExclusiveOrAssign:
+            case ExpressionType.LeftShiftAssign:
+            case ExpressionType.ModuloAssign:
+            case ExpressionType.MultiplyAssign:
+            case ExpressionType.OrAssign:
+            case ExpressionType.PowerAssign:
+            case ExpressionType.RightShiftAssign:
+            case ExpressionType.SubtractAssign:
+            case ExpressionType.AddAssignChecked:
+            case ExpressionType.MultiplyAssignChecked:
+            case ExpressionType.SubtractAssignChecked:
+            case ExpressionType.PreIncrementAssign:
+            case ExpressionType.PreDecrementAssign:
+            case ExpressionType.PostIncrementAssign:
+            case ExpressionType.PostDecrementAssign:
+            case ExpressionType.TypeEqual:
+            case ExpressionType.OnesComplement:
+            case ExpressionType.IsTrue:
+            case ExpressionType.IsFalse:
+            default:
+                throw new NotSupportedException();
+        }
+
+
     }
 
     private static IEnumerable<IExpressionEnumeratorItem> GetExpressionChains(Expression node)
     {
-        List<Expression> stack = new List<Expression>();
-        return GetExpressionsCore(stack, node);
+        return GetExpressionsCore(new List<Expression>(), node);
 
         static IEnumerable<IExpressionEnumeratorItem> GetExpressionsCore(List<Expression> stack, Expression expression, bool reorderedChain = false)
         {
-            if (expression.NodeType == ExpressionType.Call && !reorderedChain)
+            if ((expression.NodeType == ExpressionType.Call || expression.NodeType == ExpressionType.MemberAccess) && !reorderedChain)
             {
-                var callExpression = (MethodCallExpression)expression;
-
-                if (callExpression.Method.DeclaringType == typeof(Queryable) || callExpression.Method.DeclaringType == typeof(Enumerable))
+                var chainItems = GetReorderedChainCall(expression).ToArray();
+                foreach (var chainItem in chainItems)
                 {
-                    var chainItems = GetOrderedQuerableChainCall(expression).ToArray();
-                    foreach (var chainItem in chainItems)
-                    {
-                        foreach (var item in GetExpressionsCore(stack, chainItem, true))
-                            yield return item;
-                        stack.Add(chainItem);
-                    }
-                    stack.RemoveRange(stack.Count - chainItems.Length, chainItems.Length);
-
-                    //foreach (var item in GetExpressionsCore(stack, callExpression.Arguments.First()))
-                    //    yield return item;
-                    //using (new StackPusher(stack, callExpression.Arguments.First()))
-                    //using (new StackPusher(stack, expression))
-                    //    foreach (var argExpression in callExpression.Arguments.Skip(1))
-                    //        foreach (var item in GetExpressionsCore(stack, argExpression))
-                    //            yield return item;
+                    foreach (var item in GetExpressionsCore(stack, chainItem, true))
+                        yield return item;
+                    stack.Add(chainItem);
                 }
-                else
-                    throw new NotSupportedException();
+                stack.RemoveRange(stack.Count - chainItems.Length, chainItems.Length);
+
                 yield break;
             }
 
             using (new StackPusher(stack, expression))
                 switch (expression.NodeType)
                 {
+                    case ExpressionType.MemberAccess:
                     case ExpressionType.Parameter:
                     case ExpressionType.Constant:
                         break;
@@ -88,19 +181,12 @@ class SqlQueryTranslator
                         foreach (var item in GetExpressionsCore(stack, binaryExpression.Right))
                             yield return item;
                         break;
-                    case ExpressionType.MemberAccess:
-                        var memberExpression = (MemberExpression)expression;
-                        foreach (var item in GetExpressionsCore(stack, memberExpression.Expression))
-                            yield return item;
-                        break;
                     case ExpressionType.Call:
                         var callExpression = (MethodCallExpression)expression;
                         if (callExpression.Method.DeclaringType == typeof(Queryable) || callExpression.Method.DeclaringType == typeof(Enumerable))
-                        {
                             foreach (var argExpression in callExpression.Arguments.Skip(1))
                                 foreach (var item in GetExpressionsCore(stack, argExpression))
                                     yield return item;
-                        }
                         else
                             throw new NotSupportedException();
                         break;
@@ -182,7 +268,6 @@ class SqlQueryTranslator
                     case ExpressionType.OnesComplement:
                     case ExpressionType.IsTrue:
                     case ExpressionType.IsFalse:
-                        throw new NotSupportedException();
                     default:
                         throw new NotSupportedException();
                 }
@@ -191,7 +276,7 @@ class SqlQueryTranslator
         }
     }
 
-    public static string GetTableNameAndEntityType(ConstantExpression constantExpression)
+    public static string GetTableName(ConstantExpression constantExpression)
     {
         var constantValue = constantExpression.Value;
         if (constantValue.GetType().GetGenericTypeDefinition() != typeof(System.Data.Linq.Table<>))
@@ -203,13 +288,14 @@ class SqlQueryTranslator
     }
 
 
-    private static IEnumerable<Expression> GetOrderedQuerableChainCall(Expression expression)
+    private static IEnumerable<Expression> GetReorderedChainCall(Expression expression)
     {
         List<Expression> toReturn = new List<Expression>();
         while (true)
         {
             switch (expression.NodeType)
             {
+                case ExpressionType.Parameter:
                 case ExpressionType.Constant:
                     toReturn.Add(expression);
                     toReturn.Reverse();
@@ -223,6 +309,11 @@ class SqlQueryTranslator
                     }
                     else
                         throw new NotSupportedException();
+                    break;
+                case ExpressionType.MemberAccess:
+                    var memberExpression = (MemberExpression)expression;
+                    toReturn.Add(expression);
+                    expression = memberExpression.Expression;
                     break;
                 default:
                     throw new NotSupportedException();
