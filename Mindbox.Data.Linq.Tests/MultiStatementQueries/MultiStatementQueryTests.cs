d3d25294ce5b84968a69e120ccff64256b543de0
Timur Mannapov <mannapov@mindbox.cloud>
2023-07-21 20:03:02 +0400

    Draft

--- Mindbox.Data.Linq.Tests/MultiStatementQueries/MultiStatementQueryTests.cs
+++ Mindbox.Data.Linq.Tests/MultiStatementQueries/MultiStatementQueryTests.cs
@@ -152,114 +152,114 @@ public class MultiStatementQueryTests
         query.CommandText.MatchSnapshot();
     }
 
-    [TestMethod]
-    public void Translate_TableLinkedViaReferenceChainAssociation_Success()
-    {
-        // Arrange
-        using var contextAndConnection = new DataContextAndConnection();
+    //[TestMethod]
+    //public void Translate_TableLinkedViaReferenceChainAssociation_Success()
+    //{
+    //    // Arrange
+    //    using var contextAndConnection = new DataContextAndConnection();
 
-        // Act
-        var queryExpression = contextAndConnection.DataContext
-            .GetTable<Customer>().Where(c => c.Area.SubArea.Name == "SomeSubArea").Expression;
-        var query = SqlQueryTranslator.Transalate(queryExpression, new DbColumnTypeProvider());
+    //    // Act
+    //    var queryExpression = contextAndConnection.DataContext
+    //        .GetTable<Customer>().Where(c => c.Area.SubArea.Name == "SomeSubArea").Expression;
+    //    var query = SqlQueryTranslator.Transalate(queryExpression, new DbColumnTypeProvider());
 
-        // Assert
-        query.CommandText.MatchSnapshot();
-    }
+    //    // Assert
+    //    query.CommandText.MatchSnapshot();
+    //}
 
-    [TestMethod]
-    public void Translate_TableLinkedViaReferenceChainAssociationAndFilterOnAllAssociations_Success()
-    {
-        // Arrange
-        using var contextAndConnection = new DataContextAndConnection();
+    //[TestMethod]
+    //public void Translate_TableLinkedViaReferenceChainAssociationAndFilterOnAllAssociations_Success()
+    //{
+    //    // Arrange
+    //    using var contextAndConnection = new DataContextAndConnection();
 
-        // Act
-        var queryExpression = contextAndConnection.DataContext
-            .GetTable<Customer>()
-            .Where(c => c.Area.Name == "SomeArea")
-            .Where(c => c.Area.SubArea.Name == "SomeSubArea").Expression;
-        var query = SqlQueryTranslator.Transalate(queryExpression, new DbColumnTypeProvider());
+    //    // Act
+    //    var queryExpression = contextAndConnection.DataContext
+    //        .GetTable<Customer>()
+    //        .Where(c => c.Area.Name == "SomeArea")
+    //        .Where(c => c.Area.SubArea.Name == "SomeSubArea").Expression;
+    //    var query = SqlQueryTranslator.Transalate(queryExpression, new DbColumnTypeProvider());
 
-        // Assert
-        query.CommandText.MatchSnapshot();
-    }
+    //    // Assert
+    //    query.CommandText.MatchSnapshot();
+    //}
 
-    [TestMethod]
-    public void Translate_TableJoinByDataFieldViaWhere_Success()
-    {
-        // Arrange
-        using var contextAndConnection = new DataContextAndConnection();
+    //[TestMethod]
+    //public void Translate_TableJoinByDataFieldViaWhere_Success()
+    //{
+    //    // Arrange
+    //    using var contextAndConnection = new DataContextAndConnection();
 
-        // Act
-        var customerActions = contextAndConnection.DataContext.GetTable<CustomerAction>();
-        var queryExpression = contextAndConnection.DataContext
-            .GetTable<Customer>()
-            .Where(c => customerActions.Where(ca => ca.CustomerId == c.Id).Any(ca => ca.ActionTemplateId == 10))
-            .Expression;
-        var query = SqlQueryTranslator.Transalate(queryExpression, new DbColumnTypeProvider());
+    //    // Act
+    //    var customerActions = contextAndConnection.DataContext.GetTable<CustomerAction>();
+    //    var queryExpression = contextAndConnection.DataContext
+    //        .GetTable<Customer>()
+    //        .Where(c => customerActions.Where(ca => ca.CustomerId == c.Id).Any(ca => ca.ActionTemplateId == 10))
+    //        .Expression;
+    //    var query = SqlQueryTranslator.Transalate(queryExpression, new DbColumnTypeProvider());
 
-        // Assert
-        query.CommandText.MatchSnapshot();
-    }
+    //    // Assert
+    //    query.CommandText.MatchSnapshot();
+    //}
 
-    [TestMethod]
-    public void Translate_TableJoinByAssociationFieldViaWhere_Success()
-    {
-        // Arrange
-        using var contextAndConnection = new DataContextAndConnection();
+    //[TestMethod]
+    //public void Translate_TableJoinByAssociationFieldViaWhere_Success()
+    //{
+    //    // Arrange
+    //    using var contextAndConnection = new DataContextAndConnection();
 
-        // Act
-        var customerActions = contextAndConnection.DataContext.GetTable<CustomerAction>();
-        var queryExpression = contextAndConnection.DataContext
-            .GetTable<Customer>()
-            .Where(c => customerActions.Where(ca => ca.ActionTemplateId == 10).Where(ca => ca.Customer == c).Any())
-            .Expression;
-        var query = SqlQueryTranslator.Transalate(queryExpression, new DbColumnTypeProvider());
+    //    // Act
+    //    var customerActions = contextAndConnection.DataContext.GetTable<CustomerAction>();
+    //    var queryExpression = contextAndConnection.DataContext
+    //        .GetTable<Customer>()
+    //        .Where(c => customerActions.Where(ca => ca.ActionTemplateId == 10).Where(ca => ca.Customer == c).Any())
+    //        .Expression;
+    //    var query = SqlQueryTranslator.Transalate(queryExpression, new DbColumnTypeProvider());
 
-        // Assert
-        query.CommandText.MatchSnapshot();
-    }
+    //    // Assert
+    //    query.CommandText.MatchSnapshot();
+    //}
 
-    [TestMethod]
-    public void Translate_TableJoinByAssociationFieldPlusDataViaWhere_Success()
-    {
-        // Arrange
-        using var contextAndConnection = new DataContextAndConnection();
+    //[TestMethod]
+    //public void Translate_TableJoinByAssociationFieldPlusDataViaWhere_Success()
+    //{
+    //    // Arrange
+    //    using var contextAndConnection = new DataContextAndConnection();
 
-        // Act
-        var customerActions = contextAndConnection.DataContext.GetTable<CustomerAction>();
-        var queryExpression = contextAndConnection.DataContext
-            .GetTable<Customer>()
-            .Where(c => customerActions.Where(ca => ca.ActionTemplateId == 10).Where(ca => ca.Customer.Id == c.Id).Any())
-            .Expression;
-        var query = SqlQueryTranslator.Transalate(queryExpression, new DbColumnTypeProvider());
+    //    // Act
+    //    var customerActions = contextAndConnection.DataContext.GetTable<CustomerAction>();
+    //    var queryExpression = contextAndConnection.DataContext
+    //        .GetTable<Customer>()
+    //        .Where(c => customerActions.Where(ca => ca.ActionTemplateId == 10).Where(ca => ca.Customer.Id == c.Id).Any())
+    //        .Expression;
+    //    var query = SqlQueryTranslator.Transalate(queryExpression, new DbColumnTypeProvider());
 
-        // Assert
-        query.CommandText.MatchSnapshot();
-    }
+    //    // Assert
+    //    query.CommandText.MatchSnapshot();
+    //}
 
-    [TestMethod]
-    public void Translate_TableJoinByAssociationFollowedBySelectMany_Success()
-    {
-        // Arrange
-        using var contextAndConnection = new DataContextAndConnection();
+    //[TestMethod]
+    //public void Translate_TableJoinByAssociationFollowedBySelectMany_Success()
+    //{
+    //    // Arrange
+    //    using var contextAndConnection = new DataContextAndConnection();
 
-        // Act
-        var orders = contextAndConnection.DataContext.GetTable<RetailOrder>();
-        var queryExpression = contextAndConnection.DataContext
-            .GetTable<Customer>()
-            .Where(c =>
-                orders.Where(o => o.CurrentCustomer == c)
-                   .SelectMany(o => o.History.Single(hi => hi.IsCurrentOtherwiseNull != null).Purchases)
-                   .Where(p => p.PriceForCustomerOfLine / p.Count != null && p.PriceForCustomerOfLine / p.Count >= 123)
-                   .Any()
-            )
-            .Expression;
-        var query = SqlQueryTranslator.Transalate(queryExpression, new DbColumnTypeProvider());
+    //    // Act
+    //    var orders = contextAndConnection.DataContext.GetTable<RetailOrder>();
+    //    var queryExpression = contextAndConnection.DataContext
+    //        .GetTable<Customer>()
+    //        .Where(c =>
+    //            orders.Where(o => o.CurrentCustomer == c)
+    //               .SelectMany(o => o.History.Single(hi => hi.IsCurrentOtherwiseNull != null).Purchases)
+    //               .Where(p => p.PriceForCustomerOfLine / p.Count != null && p.PriceForCustomerOfLine / p.Count >= 123)
+    //               .Any()
+    //        )
+    //        .Expression;
+    //    var query = SqlQueryTranslator.Transalate(queryExpression, new DbColumnTypeProvider());
 
-        // Assert
-        query.CommandText.MatchSnapshot();
-    }
+    //    // Assert
+    //    query.CommandText.MatchSnapshot();
+    //}
 
     // Several neested joins
     // Querable join
