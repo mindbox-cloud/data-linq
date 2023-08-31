41200ccbadca0b2ed66a87dbb031d6ca5c915e64
TimurMannapov <mannapov@mindbox.cloud>
2023-09-01 03:34:35 +0400

    Draft

--- Mindbox.Data.Linq.Tests/MultiStatementQueries/SqlQueryTranslator.cs
+++ Mindbox.Data.Linq.Tests/MultiStatementQueries/SqlQueryTranslator.cs
@@ -1,8 +1,6 @@
-﻿using Castle.Components.DictionaryAdapter.Xml;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
 
@@ -386,7 +384,7 @@ class SqlQueryTranslator
     }
 
     /// <summary>
-    /// Operator against constaint.
+    /// Operator against constant.
     /// We can simply analysis only non constant part and drop constant sle.
     /// Examples: 'c.Password == "asdf"' or 'c.Age > 20' or 'c.CustomerActions.Where(c=>c.ActionTypeId == 12).Count() < 10'
     /// </summary>
@@ -491,271 +489,8 @@ class SqlQueryTranslator
             return name;
         }
     }
-
-
-}
-/*
-class MultiStatementQuery
-{
-    private List<TableNode> _tables = new();
-
-    public IReadOnlyList<TableNode> Tables => _tables;
-
-    public TableNode AddTable(string rightTableName)
-    {
-        _tables.Add(new TableNode(rightTableName));
-        return _tables.Last();
-    }
-
-    public IEnumerable<(TableNode Removed, TableNode ReplacedBy)> OptmizeQuery()
-    {
-        foreach (var table in _tables.Skip(1))
-            if (table.JoinConditions.Count == 0)
-                throw new InvalidOperationException("Join conditions missiong for table.");
-
-        return RemoveNeedlessBackLookups().Concat(RemoveDuplicatedJoins());
-    }
-
-    private IEnumerable<(TableNode Removed, TableNode ReplacedBy)> RemoveNeedlessBackLookups()
-    {
-        List<(TableNode Removed, TableNode ReplacedBy)> toReturn = new();
-        while (true)
-        {
-            var toRemoveInfo = GetToRemove();
-            if (!toRemoveInfo.HasValue)
-                break;
-            foreach (var usedColumn in toRemoveInfo.Value.ToRemove.UsedColumns)
-                toRemoveInfo.Value.Replacement.AddUsedField(usedColumn);
-            _tables.Remove(toRemoveInfo.Value.ToRemove);
-            foreach (var table in _tables)
-                table.ReplaceTable(toRemoveInfo.Value.ToRemove, toRemoveInfo.Value.Replacement);
-
-            toReturn.Add(toRemoveInfo.Value);
-        }
-        return toReturn;
-
-        (TableNode ToRemove, TableNode Replacement)? GetToRemove()
-        {
-            var tablesToCheck = _tables.Where(t => t.JoinConditions.Select(c => c.LeftTable).Distinct().Count() == 1).ToArray();
-
-            for (int i = 0; i < tablesToCheck.Length - 1; i++)
-            {
-                var replacement = tablesToCheck[i];
-                for (int j = i + 1; j < tablesToCheck.Length; j++)
-                {
-                    var toRemove = tablesToCheck[j];
-                    if (!IsSameJoin(replacement, toRemove))
-                        continue;
-
-                    if (toRemove.JoinConditions.Select(t => t.LeftTable).Distinct().Single() != replacement)
-                        continue;
-
-                    return (toRemove, replacement.JoinConditions.Select(t => t.LeftTable).Distinct().Single());
-                }
-            }
-            return null;
-        }
-
-        bool IsSameJoin(TableNode tableA, TableNode tableB)
-        {
-            var conditionA = tableA.JoinConditions.Single();
-            var conditionB = tableB.JoinConditions.Single();
-
-            if (conditionA.LeftTable.TableName != tableB.TableName || conditionB.LeftTable.TableName != tableA.TableName)
-                return false;
-
-            if (conditionA.FieldLeft != conditionB.FieldRight || conditionA.FieldRight != conditionB.FieldLeft)
-                return false;
-            return true;
-        }
-    }
-
-    private IEnumerable<(TableNode Removed, TableNode ReplacedBy)> RemoveDuplicatedJoins()
-    {
-        List<(TableNode Removed, TableNode ReplacedBy)> toReturn = new();
-        while (true)
-        {
-            var toRemoveInfo = GetToRemove();
-            if (!toRemoveInfo.HasValue)
-                break;
-            foreach (var usedColumn in toRemoveInfo.Value.ToRemove.UsedColumns)
-                toRemoveInfo.Value.Replacement.AddUsedField(usedColumn);
-            _tables.Remove(toRemoveInfo.Value.ToRemove);
-            foreach (var table in _tables)
-                table.ReplaceTable(toRemoveInfo.Value.ToRemove, toRemoveInfo.Value.Replacement);
-
-            toReturn.Add(toRemoveInfo.Value);
-        }
-        return toReturn;
-
-        (TableNode ToRemove, TableNode Replacement)? GetToRemove()
-        {
-            for (int i = _tables.Count - 1; i > 0; i--)
-            {
-                var toRemoveTable = _tables[i];
-                for (int j = 0; j < i; j++)
-                {
-                    var replacementTable = _tables[j];
-                    if (toRemoveTable.TableName != replacementTable.TableName)
-                        continue;
-                    if (toRemoveTable.JoinConditions.Count != replacementTable.JoinConditions.Count)
-                        continue;
-                    if (toRemoveTable.JoinConditions.Except(replacementTable.JoinConditions).Any() ||
-                        replacementTable.JoinConditions.Except(toRemoveTable.JoinConditions).Any())
-                        continue;
-                    return (toRemoveTable, replacementTable);
-                }
-            }
-            return null;
-        }
-    }
-
-    public string Dump()
-    {
-        var sb = new StringBuilder();
-        foreach (var table in _tables)
-        {
-            var columns = string.Empty;
-            if (table.UsedColumns.Count > 0)
-                columns = $"({string.Join(", ", table.UsedColumns)})";
-            sb.AppendLine($"{table.TableName}{columns}");
-            if (table.JoinConditions.Count > 0)
-                foreach (var condition in table.JoinConditions)
-                    sb.AppendLine($"\t{condition.FieldRight} = {condition.LeftTable.TableName}.{condition.FieldLeft}");
-        }
-
-        return sb.ToString();
-    }
 }
 
-[DebuggerDisplay("{TableName}")]
-class TableNode
-{
-    private List<string> _usedColumns = new();
-    private List<JoinCondition> _joinConditions = new();
-
-    public string TableName { get; }
-    public IReadOnlyList<string> UsedColumns => _usedColumns;
-    public IReadOnlyList<JoinCondition> JoinConditions => _joinConditions;
-
-    public TableNode(string tableName)
-    {
-        TableName = tableName;
-    }
-
-    public void AddUsedField(string name)
-    {
-        if (_usedColumns.Contains(name))
-            return;
-        _usedColumns.Add(name);
-    }
-
-    public void AddJoinCondition(JoinCondition condition)
-    {
-        if (_joinConditions.Contains(condition))
-            throw new ArgumentException("Condition already added.");
-        if (condition.LeftTable == this)
-            throw new ArgumentException("LeftTable is same as right table in join.");
-        _joinConditions.Add(condition);
-        condition.LeftTable.AddUsedField(condition.FieldLeft);
-        AddUsedField(condition.FieldRight);
-    }
-
-    internal void ReplaceTable(TableNode toReplace, TableNode replacement)
-    {
-        for (int i = 0; i < _joinConditions.Count; i++)
-            if (_joinConditions[i].LeftTable == toReplace)
-                _joinConditions[i] = new JoinCondition(_joinConditions[i].FieldRight, replacement, _joinConditions[i].FieldLeft);
-    }
-}
-
-record JoinCondition(string FieldRight, TableNode LeftTable, string FieldLeft);
-
-class TranslationContext
-{
-    private Dictionary<Expression, TableNode> _expressionToTableMapping = new();
-    private Dictionary<TableNode, List<Expression>> _tableFilters = new();
-
-    public MultiStatementQuery Query { get; } = new();
-    public TableNode CurrentTable { get; private set; }
-    public IDbColumnTypeProvider ColumnTypeProvider { get; }
-
-    public TranslationContext(IDbColumnTypeProvider columnTypeProvider)
-    {
-        ColumnTypeProvider = columnTypeProvider;
-    }
-
-    public TableNode AddTable(string tableName, Expression expression)
-    {
-        if (expression != null && _expressionToTableMapping.TryGetValue(expression, out var table))
-        {
-            if (table.TableName != tableName)
-                throw new InvalidOperationException("Same expression mapped to different tables. Error in implementation.");
-            CurrentTable = table;
-        }
-        else
-        {
-            CurrentTable = Query.AddTable(tableName);
-            if (expression != null)
-                _expressionToTableMapping.Add(expression, CurrentTable);
-        }
-        return CurrentTable;
-    }
-
-    public void AddTableFilter(Expression tableFilter)
-    {
-        if (!_tableFilters.TryGetValue(CurrentTable, out var filters))
-        {
-            filters = new List<Expression>();
-            _tableFilters.Add(CurrentTable, filters);
-        }
-        if (!filters.Contains(tableFilter))
-            filters.Add(tableFilter);
-    }
-
-    public void SetCurrentTable(TableNode table)
-    {
-        if (Query == null)
-            throw new InvalidOperationException("Query not yet constructed.");
-        CurrentTable = table;
-    }
-
-    public void MapParameterToTable(ParameterExpression parameterExpression)
-    {
-        if (CurrentTable == null)
-            throw new ArgumentException();
-        if (_expressionToTableMapping.TryGetValue(parameterExpression, out var tableMapping))
-        {
-            if (tableMapping != CurrentTable)
-                throw new InvalidOperationException("Same expression mapped to different tables. Error in implementation.");
-            return;
-        }
-        _expressionToTableMapping.Add(parameterExpression, CurrentTable);
-    }
-
-    public void OptmizeQuery()
-    {
-        foreach (var entry in Query.OptmizeQuery())
-            foreach (var expression in _expressionToTableMapping.Where(i => i.Value == entry.Removed).Select(s => s.Key).ToArray())
-            {
-                _expressionToTableMapping.Remove(expression);
-                _expressionToTableMapping.Add(expression, entry.ReplacedBy);
-            }
-    }
-
-    public TableNode GetTableFromExpression(Expression parameterExpression)
-        => _expressionToTableMapping[parameterExpression];
-
-    public void ResetCurrentTable() => CurrentTable = null;
-
-    internal void FillJoinConditions()
-    {
-        foreach (var (table, filters) in _tableFilters)
-            foreach (var joinCondition in ExpressionHelpers.GetJoinConditions(this, table, filters))
-                table.AddJoinCondition(joinCondition);
-    }
-}
-*/
 
 [DebuggerDisplay("{Name}")]
 class TableNode2
@@ -818,7 +553,7 @@ class TableNode2
                     connectionA.OtherTable.AddConnection(connectionBOtherTableConnection.TableFields, connectionBOtherTableConnection.OtherTable, connectionBOtherTableConnection.OtherTableFields);
             }
 
-        // if we have connections of like Customer->CustomerAction->Customer->SomeOtherTable
+        // if we have connections like Customer->CustomerAction->Customer->SomeOtherTable
         // We can actually remove second Customer connection(move all its connections to top Customer) and have something like this
         // Custmoer -> CustomerAction
         //        \ -> SomeOtherTable
@@ -923,7 +658,7 @@ class Connection
     }
 
     /// <summary>
-    /// Shows that it is same connection exactly or reveresed connection.
+    /// Shows that it is same connection exactly or reversed connection.
     /// </summary>
     /// <param name="connection">Connection to check.</param>
     /// <returns>True - same or same-reversed, false - not same.</returns>
