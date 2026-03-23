b71bbb647e4cd28575ae9410bf3b08cc0ff63e5d
Aleksandr Korchak <korchak@mindbox.cloud>
2026-03-23 15:23:34 +0000

    refactor: add Directory.Build/Packages.props, restore SecurityUtils, fix CI
    - Add Directory.Build.props: TargetFramework=net8.0, LangVersion=12
    - Add Directory.Packages.props: centralized package versions
    - Restore original SecurityUtils.cs (add System.Security.Permissions 8.0.0
      package; suppress SYSLIB0003 — CAS is a no-op on .NET 5+, types still
      compile with the compat package)
    - Simplify csproj files: remove properties now in Directory.Build.props
    - Update pull-request.yml: setup-dotnet@v4, dotnet-version 8.0.x

--- Mindbox.Data.Linq/misc/SecurityUtils.cs
+++ Mindbox.Data.Linq/misc/SecurityUtils.cs
@@ -1,79 +1,214 @@
+//------------------------------------------------------------------------------
+// <copyright file="SecurityUtils.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
 using System;
 using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
 
 namespace System.Data.Linq
 {
-    // Code Access Security (ReflectionPermission) is not supported on .NET 5+.
-    // This simplified version removes CAS checks which were no-ops on modern .NET.
-    internal static class SecurityUtils
-    {
-        internal static object SecureCreateInstance(Type type)
+    /// <devdoc>
+    ///     Useful methods to securely call 'dangerous' managed APIs (especially reflection).
+    ///     See http://wiki/default.aspx/Microsoft.Projects.DotNetClient.SecurityConcernsAroundReflection
+    ///     for more information specifically about why we need to be careful about reflection invocations.
+    /// </devdoc>
+    internal static class SecurityUtils {
+
+        private static volatile ReflectionPermission memberAccessPermission = null;
+        private static volatile ReflectionPermission restrictedMemberAccessPermission = null;
+
+        private static ReflectionPermission MemberAccessPermission
         {
+            get {
+                if (memberAccessPermission == null) {
+                    memberAccessPermission = new ReflectionPermission(ReflectionPermissionFlag.MemberAccess);
+                }
+                return memberAccessPermission;
+            }
+        }
+
+        private static ReflectionPermission RestrictedMemberAccessPermission {
+            get {
+                if (restrictedMemberAccessPermission == null) {
+                    restrictedMemberAccessPermission = new ReflectionPermission(ReflectionPermissionFlag.RestrictedMemberAccess);
+                }
+                return restrictedMemberAccessPermission;
+            }
+        }
+
+        private static void DemandReflectionAccess(Type type) {
+			MemberAccessPermission.Demand();
+		}
+
+        private static bool HasReflectionPermission(Type type) {
+            try {
+                DemandReflectionAccess(type);
+                return true;
+            }
+            catch (SecurityException) {
+            }
+
+            return false;
+        }
+
+       
+        /// <devdoc>
+        ///     This helper method provides safe access to Activator.CreateInstance.
+        ///     NOTE: This overload will work only with public .ctors. 
+        /// </devdoc>
+        internal static object SecureCreateInstance(Type type) {
             return SecureCreateInstance(type, null, false);
         }
 
-        internal static object SecureCreateInstance(Type type, object[] args, bool allowNonPublic)
-        {
-            if (type == null)
+
+        /// <devdoc>
+        ///     This helper method provides safe access to Activator.CreateInstance.
+        ///     Set allowNonPublic to true if you want non public ctors to be used. 
+        /// </devdoc>
+        internal static object SecureCreateInstance(Type type, object[] args, bool allowNonPublic) {
+            if (type == null) {
                 throw new ArgumentNullException("type");
+            }
 
             BindingFlags flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.CreateInstance;
-            if (allowNonPublic)
+           
+            // if it's an internal type, we demand reflection permission.
+            if (!type.IsVisible) {
+                DemandReflectionAccess(type);
+            }
+            else if (allowNonPublic && !HasReflectionPermission(type)) {
+                // Someone is trying to instantiate a public type in *our* assembly, but does not
+                // have full reflection permission. We shouldn't pass BindingFlags.NonPublic in this case.
+                // The reason we don't directly demand the permission here is because we don't know whether
+                // a public or non-public .ctor will be invoked. We want to allow the public .ctor case to
+                // succeed.
+                allowNonPublic = false;
+            }
+            
+            if (allowNonPublic) {
                 flags |= BindingFlags.NonPublic;
+            }
 
             return Activator.CreateInstance(type, flags, null, args, null);
         }
 
-        internal static object SecureCreateInstance(Type type, object[] args)
-        {
+        /// <devdoc>
+        ///     This helper method provides safe access to Activator.CreateInstance.
+        ///     NOTE: This overload will work only with public .ctors. 
+        /// </devdoc>
+        internal static object SecureCreateInstance(Type type, object[] args) {
             return SecureCreateInstance(type, args, false);
         }
 
-        internal static object SecureConstructorInvoke(Type type, Type[] argTypes, object[] args, bool allowNonPublic)
-        {
+
+        /// <devdoc>
+        ///     Helper method to safely invoke a .ctor. You should prefer SecureCreateInstance to this.
+        ///     Set allowNonPublic to true if you want non public ctors to be used. 
+        /// </devdoc>
+        internal static object SecureConstructorInvoke(Type type, Type[] argTypes, object[] args, bool allowNonPublic) {
             return SecureConstructorInvoke(type, argTypes, args, allowNonPublic, BindingFlags.Default);
         }
 
-        internal static object SecureConstructorInvoke(Type type, Type[] argTypes, object[] args,
-                                                       bool allowNonPublic, BindingFlags extraFlags)
-        {
-            if (type == null)
+        /// <devdoc>
+        ///     Helper method to safely invoke a .ctor. You should prefer SecureCreateInstance to this.
+        ///     Set allowNonPublic to true if you want non public ctors to be used. 
+        ///     The 'extraFlags' parameter is used to pass in any other flags you need, 
+        ///     besides Public, NonPublic and Instance.
+        /// </devdoc>
+        internal static object SecureConstructorInvoke(Type type, Type[] argTypes, object[] args, 
+                                                       bool allowNonPublic, BindingFlags extraFlags) {
+            if (type == null) {
                 throw new ArgumentNullException("type");
+            }
+   
+            // if it's an internal type, we demand reflection permission.
+            if (!type.IsVisible) {
+                DemandReflectionAccess(type);
+            }
+            else if (allowNonPublic && !HasReflectionPermission(type)) {
+                // Someone is trying to invoke a ctor on a public type, but does not
+                // have full reflection permission. We shouldn't pass BindingFlags.NonPublic in this case.
+                allowNonPublic = false;
+            }
 
             BindingFlags flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | extraFlags;
-            if (!allowNonPublic)
+            if (!allowNonPublic) {
                 flags &= ~BindingFlags.NonPublic;
+            }
 
             ConstructorInfo ctor = type.GetConstructor(flags, null, argTypes, null);
-            if (ctor != null)
+            if (ctor != null) {
                 return ctor.Invoke(args);
+            }
 
             return null;
         }
 
-        internal static object FieldInfoGetValue(FieldInfo field, object target)
-        {
-            if (field.DeclaringType == null)
+        private static bool GenericArgumentsAreVisible(MethodInfo method) {
+            if (method.IsGenericMethod) {
+                Type[] parameterTypes = method.GetGenericArguments();
+                foreach (Type type in parameterTypes) {
+                    if (!type.IsVisible) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+       
+        /// <devdoc>
+        ///     This helper method provides safe access to FieldInfo's GetValue method.
+        /// </devdoc>
+        internal static object FieldInfoGetValue(FieldInfo field, object target) {
+            Type type = field.DeclaringType;
+            if (type == null) {
+                // Type is null for Global fields.
                 throw new NotImplementedException("Global fields are not supported.");
-
+            } else if (!(type != null && type.IsVisible && field.IsPublic)) {
+                DemandReflectionAccess(type);
+            }
             return field.GetValue(target);
         }
 
-        internal static object MethodInfoInvoke(MethodInfo method, object target, object[] args)
-        {
-            if (method.DeclaringType == null)
-                throw new NotImplementedException("Global methods are not supported.");
-
+        /// <devdoc>
+        ///     This helper method provides safe access to MethodInfo's Invoke method.
+        /// </devdoc>
+        internal static object MethodInfoInvoke(MethodInfo method, object target, object[] args) {
+            Type type = method.DeclaringType;
+            if (type == null) {
+				// Type is null for Global methods. In this case we would need to demand grant set on 
+				// the containing assembly for internal methods.
+				throw new NotImplementedException("Global methods are not supported.");
+			} else if (!(type.IsVisible && method.IsPublic && GenericArgumentsAreVisible(method))) {
+                // this demand is required for internal types in system.dll and its friend assemblies. 
+                DemandReflectionAccess(type);
+            }
             return method.Invoke(target, args);
         }
 
-        internal static object ConstructorInfoInvoke(ConstructorInfo ctor, object[] args)
-        {
+        /// <devdoc>
+        ///     This helper method provides safe access to ConstructorInfo's Invoke method.
+        ///     Constructors can't be generic, so we don't check if argument types are visible
+        /// </devdoc>
+        internal static object ConstructorInfoInvoke(ConstructorInfo ctor, object[] args) {
+            Type type = ctor.DeclaringType;
+            if ((type != null) && !(type.IsVisible && ctor.IsPublic)) {
+                DemandReflectionAccess(type);
+            }
             return ctor.Invoke(args);
         }
 
-        internal static object ArrayCreateInstance(Type type, int length)
-        {
+        /// <devdoc>
+        ///     This helper method provides safe access to Array.CreateInstance.
+        /// </devdoc>
+        internal static object ArrayCreateInstance(Type type, int length) {
+            if (!type.IsVisible) {
+                DemandReflectionAccess(type);
+            }
             return Array.CreateInstance(type, length);
         }
     }
